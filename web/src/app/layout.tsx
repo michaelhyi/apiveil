b1efcd702375a09b0bb7f0e69b914e59d991bcf1
michaelhyi <michaelyi2004@gmail.com>
2025-04-08 16:34:32 -0400

    installed nextjs

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "APIVeil",
+  description: "CS 4723-XCS: Team 13",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
