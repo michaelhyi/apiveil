371cb65d0316e3ed6ad98b1f98b7d18336212d4f
michaelhyi <michaelyi2004@gmail.com>
2025-04-09 01:23:35 -0400

    implemented metadata view

--- web/src/app/proxy/[proxyId]/page.tsx
+++ web/src/app/proxy/[proxyId]/page.tsx
@@ -2,27 +2,16 @@
 
 import Loading from "@/components/Loading";
 import ProtectedRoute from "@/components/ProtectedRoute";
+import ProxyLog from "@/components/ProxyLog";
 import ProxyHttpClient from "@/http/ProxyHttpClient";
 import { ProxyWithLogs } from "@/types/ProxyWithLogs";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-function convertUnixToFormattedDate(timestamp: string): string {
-    const date = new Date(timestamp);
-    return date.toLocaleString("en-US", {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: true,
-    });
-}
-
 export default function ProxyPage({ params }: { params: { proxyId: string } }) {
     const [loading, setLoading] = useState<boolean>(true);
     const [data, setData] = useState<ProxyWithLogs | null>(null);
+    const [expanded, setExpanded] = useState<number>(-1);
 
     useEffect(() => {
         (async () => {
@@ -139,7 +128,7 @@ export default function ProxyPage({ params }: { params: { proxyId: string } }) {
                             <span className="font-medium">Base API URL:</span>
                             &nbsp;
                             <span className="text-[#A5A2A2]">
-                                https://infra-api-gateway-3b3fe1b4-9ae7.company.com/
+                                {data.apiBaseUrl}
                             </span>
                         </p>
                     </div>
@@ -149,31 +138,12 @@ export default function ProxyPage({ params }: { params: { proxyId: string } }) {
                         className="mt-18 border border-[#A2A5A5] rounded-lg h-128 overflow-y-auto p-3"
                     >
                         {data.logs.map((log) => (
-                            <div
+                            <ProxyLog
                                 key={log.proxyLogId}
-                                className="flex items-center gap-3 text-sm mt-4 ml-6"
-                            >
-                                <p>&rsaquo;</p>
-                                <p>
-                                    {convertUnixToFormattedDate(log.timestamp)}
-                                </p>
-                                <p className="text-white">
-                                    {log.method}&nbsp;{log.path}
-                                </p>
-                                <p
-                                    className={`font-semibold px-2 rounded-md ${
-                                        log.statusCode >= 200 &&
-                                        log.statusCode < 300
-                                            ? "text-[#89DA9F] bg-[#143518]"
-                                            : log.statusCode >= 400 &&
-                                                log.statusCode <= 500
-                                              ? "text-[#EFA296] bg-[#521F0F]"
-                                              : "text-gray-500"
-                                    }`}
-                                >
-                                    {log.statusCode}
-                                </p>
-                            </div>
+                                log={log}
+                                expanded={expanded}
+                                setExpanded={setExpanded}
+                            />
                         ))}
                     </div>
                 </main>
